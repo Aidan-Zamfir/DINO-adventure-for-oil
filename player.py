2a507602704ba802d9a37eb14c9ea32858d6ebde
Aidan-Z <aidanzamfir@gmail.com>
2024-04-25 10:38:27 -0300

    duck

--- player.py
+++ player.py
@@ -18,6 +18,8 @@ JUMP = [pygame.image.load(os.path.join("Assets/Dino", "DinoJump.png"))]
 class Dino:
     X_POS = 80
     Y_POS = 310
+    Y_POS_DUCK = 340
+    JUMP_VELOCITY = 8.5
 
     def __init__(self):
         self.duck_img = DUCK
@@ -29,6 +31,7 @@ class Dino:
         self.dino_jump = False
 
         self.step_index = 0
+        self.jump_velocity = self.JUMP_VELOCITY
         self.image = self.run_img[0]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = self.X_POS
@@ -60,6 +63,13 @@ class Dino:
             self.dino_run = True
             self.dino_jump = False
 
+    def duck(self):
+        self.image = self.duck_img[self.step_index // 5]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS_DUCK
+        self.step_index += 1
+        self.jump_velocity = self.JUMP_VELOCITY
 
     def run(self):
         self.image = self.run_img[self.step_index // 5]
@@ -67,6 +77,15 @@ class Dino:
         self.dino_rect.x = self.X_POS
         self.dino_rect.y = self.Y_POS
         self.step_index += 1
+
+    def jump(self):
+        self.image = self.jump_img
+        if self.dino_jump:
+            self.dino_rect.y -= self.jump_velocity * 4
+            self.jump_velocity -= 0.8
+        if self.jump_velocity < - self.JUMP_VELOCITY:
+            self.dino_jump = False
+            self.jump_velocity = self.JUMP_VELOCITY
 
     def draw(self, SCREEN):
         SCREEN.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
